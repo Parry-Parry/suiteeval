d7553bde98dc052df044001d0e7901517cdf75c0
“Andrew <a.parry.1@research.gla.ac.uk>
2025-10-22 11:55:38 +0100

    feat: Add bright to __init__.py

--- suiteeval/suite/__init__.py
+++ suiteeval/suite/__init__.py
@@ -3,6 +3,7 @@ from suiteeval.suite.beir import BEIR
 from suiteeval.suite.lotte import Lotte
 from suiteeval.suite.msmarco import MSMARCODocument, MSMARCOPassage
 from suiteeval.suite.nanobeir import NanoBEIR
+from suiteeval.suite.bright import BRIGHT
 
 __all__ = [
     "Suite",
@@ -11,4 +12,5 @@ __all__ = [
     "MSMARCODocument",
     "MSMARCOPassage",
     "NanoBEIR",
+    "BRIGHT",
 ]
