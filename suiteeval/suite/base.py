90b1ab1943798ca26e6c1a9cceaee29f972c97cc
“Andrew <a.parry.1@research.gla.ac.uk>
2025-11-19 16:23:23 +0000

    fix: correct method usage

--- suiteeval/suite/base.py
+++ suiteeval/suite/base.py
@@ -749,7 +749,7 @@ class Suite(ABC, metaclass=SuiteMeta):
                     save_dir = experiment_kwargs.pop("save_dir", None)
                     if save_dir is not None:
                         if not isinstance(ds_name, str):
-                            ds_name = self._get_dataset_object(ds_name)
+                            ds_name = self._get_irds_id(ds_name)
                         formatted_ds_name = ds_name.replace("/", "-").lower()
                         ds_save_dir = f"{save_dir}/{formatted_ds_name}"
                         experiment_kwargs["save_dir"] = ds_save_dir
